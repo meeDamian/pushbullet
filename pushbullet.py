ae349297379acc2d9ab62afd8bd5474ded803a07
Damian Mee <spam@meedamian.com>
2016-12-23 21:16:46 +0530

    non-dummy content added

--- pushbullet.py
+++ pushbullet.py
@@ -1,7 +1,11 @@
-import urllib2, json, decimal
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
 # set your preferred currency here
 CURRENCY = 'USD' # or 'EUR' or 'GBP'
+
+# Set this to `False` to see value in Bitcoin instead
+SATOSHI = True
 
 # create channel here: https://www.pushbullet.com/my-channel
 # NOTE: make sure to create a long and random `#tag` for it
@@ -10,18 +14,40 @@ CHANNEL = ''
 # get your PushBullet Access Token from: https://www.pushbullet.com/#settings/account
 TOKEN = ''
 
+#
+## NO NEED TO TOUCH STUFF BELOW ;)
+#
+import urllib2, json, decimal
+
 def get_price():
     content = urllib2.urlopen("https://api.coindesk.com/v1/bpi/currentprice.json").read()
     data = json.loads(content)
 
     return decimal.Decimal(data["bpi"][CURRENCY]["rate"])
 
-def notify_pushbullet(msg):
+def get_currency_sign():
+    if CURRENCY == 'USD':
+        return '$'
+    elif CURRENCY == 'EUR':
+        return '€'
+    elif CURRENCY == 'GBP':
+        return '£'
+
+def notify_pushbullet(fee, txfee):
+    earned = decimal.Decimal(fee - txfee)
+    earned_btc = earned / decimal.Decimal(100000000)
+
+    earned_str = str(earned if SATOSHI else earned_btc)
+    if SATOSHI:
+        earned_str += ' Satoshi'
+    else:
+        earned_str += 'BTC'
+
     data = {
         'channel_tag': CHANNEL,
         'type': 'note',
-        'title': 'banana',
-        'body': msg
+        'title': '{0} earned!'.format(earned_str),
+        'body': 'Which is around {0}{1:.4f} as of now'.format(get_currency_sign(), float(earned_btc * get_price()))
     }
 
     headers = {
